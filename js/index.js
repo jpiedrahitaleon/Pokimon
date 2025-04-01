e58d8ab115d624c9731fcef64ed1d981fde64cb1
Julian Piedrahita <jpiedrahitaleon@gmail.com>
2025-04-01 17:31:39 -0500

    cargue 1 abril

--- js/index.js
+++ js/index.js
@@ -2,10 +2,15 @@ function Loguear() {
     let username = document.getElementById('username').value
     let password = document.getElementById('clave').value
 
-    if(username == 'Julian' && password == '123456'){
-        alert('Usuario es valido')
-    }else{
-        alert('usuario es incorrecto')
-    }  
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    if (user == null) {
+        alert('No has iniciado sesion')
+        window.location.href = "../index.html"
+    } else if (user[3] == Usuario && user[4] == Contraseña) {
+        localStorage.setItem("user", Usuario);
+        window.location.href = "../vistas/entrenador.html"
+        alert('Usuario valido')
+    }
 
 }
--- js/registro.js
+++ js/registro.js
@@ -5,12 +5,15 @@ let Correo_electronco = document.getElementById ("email").value
 let Usuario = document.getElementById ("Usuario").value
 let Contraseña = document.getElementById ("clave").value
 let Verificar_contraseña = document.getElementById ("clave2").value
+let array = [Nombre,Apellido,Correo_electronco,Usuario,Contraseña,Verificar_contraseña]
 
-if (Contraseña == Verificar_contraseña){
-    alert("registrado")
-    let registro = [Nombre,Apellido,Correo_electronco,Usuario,Contraseña,Verificar_contraseña]
-    console.log(registro)
-}else{
-    alert ("su contraseña es diferente al recuadro de verificar contraseña")
+localStorage.setItem("user", JSON.stringify((array)));
+alert("Usuario regustrado correctamente")
+
+if (Contraseña !== Verificar_contraseña){
+    alert("Su contraseña no es la misma en los dos recuadros ")
+
 }
+
+window.location.href = "../vistas.html/entrenador.html"
 }
